3314471d0eb22e6dcd7c52354ef89af49349e436
Lara Cury <laracury2@gmail.com>
2024-10-30 18:41:10 -0300

    feat: menu completo, desktop e mobile

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,5 +1,5 @@
-//MENU RESPONSIVO
-let menuResponsivo = document.querySelector('.menu-site');
-document.querySelector('#menu').onclick =() =>{
-    menuResponsivo.classList.toggle('active');
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
 }
